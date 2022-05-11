e4957d6dd1910f4fb2b18a522498dd9c36ce6aa1
Danilo Chiarlone <39843321+danbugs@users.noreply.github.com>
2022-05-10 18:57:19 -0700

    Update lib.rs

--- rust-hello/src/lib.rs
+++ rust-hello/src/lib.rs
@@ -3,13 +3,17 @@ use spin_sdk::{
     http::{Request, Response},
     http_component,
 };
+use std::time::{SystemTime, UNIX_EPOCH};
 
 /// A simple Spin HTTP component.
 #[http_component]
 fn hello_world(_req: Request) -> Result<Response> {
-    println!("hello world from wasm module!");
-    println!("hello world from wasm module again!");
+    let nanos = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .subsec_nanos();
+    println!("nanos: {}", nanos);
     Ok(http::Response::builder()
         .status(200)
-        .body(Some("Writing a very simple Spin component in Rust AGAIN".into()))?)
+        .body(Some("Writing a very simple Spin component in Rust\n".into()))?)
 }
